f9e3704e58c0c063767f03c2804d60c5a92fe10a
The Keeper <abysskeeper@protonmail.com>
2024-08-27 23:11:11 +0330

    Remove duplicate get_note_info ca ll

--- examples/get_blockchain_info.py
+++ examples/get_blockchain_info.py
@@ -7,8 +7,7 @@ def main() -> None:
         "testnet4.pactus.org:50052",
     ).connect()
 
-    network = Network.get_node_info(connection)
-    response = network.get_node_info()
+    response = Network.get_node_info(connection)
 
     print(response)
 
